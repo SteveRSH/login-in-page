06b1905a7fcc612a15d162e3606d97d5bafe0a98
Steve Horlback <stevehorlback@steves-mbp.attlocal.net>
2017-09-10 09:55:19 -0400

    login

--- mustache.js
+++ mustache.js
@@ -1,7 +1,8 @@
-const mustacheExpress = require('mustache-express');
+
+// const static = require('serve-static');
 const express = require('express');
-const fs = require('fs');
-const app = express();
+const mustache = require('mustache-express');
+const session = require('express-session');
 const bodyparser = require('body-parser');
 const server = express();
 
@@ -11,44 +12,95 @@ const users = [
     { username: 'help', password: 'desk', logins: 0 },
 ];
 
-//Test to make sure user name matches password
-// for (let i = 0; i < users.length; i++) {
-//      if (username === users[i].username &&
-//     password === users[i].password) {
-//     user = users[i];
-//   }};
+// https://stackoverflow.com/questions/29717559/javascript-count-how-many-time-a-button-is-pressedfunction
 
+let countClicks = 0;
 
+server.engine('mustache', mustache());
+server.set('views', './views');
+server.set('view engine', 'mustache');
 server.use(bodyparser.urlencoded({
   extended: false
 }));
-server.engine('mustache', mustacheExpress());
-server.set('views', './views')
-server.set('view engine', 'mustache')
-//don't need the below because I don't have a public folder
-server.use(express.static("public"))
 
-server.get("/", function (req, res) {
-  res.render('login');
+
+server.use(session({
+   secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true
+}));
+
+
+
+server.post('/reGistration',function(req, res){
+  res.render('signup');
+})
+server.post('/signup', function(req, res){
+  const if_this_user = req.body.username;
+  const if_this_password = req.body.password;
+  users.push({
+    username: if_this_user,
+    password: if_this_password,
+
   });
-
-  server.get('/home', function (request, response) {
-  response.render('home');
+  res.render('login',{
+    totalClicks: countClicks,
+  });
 });
 
-//user name count
+server.post('/upCount', function(req, res) {
+  countClicks++;
+  res.render('login', {
+    totalClicks: countClicks,
+  });
+});
 
-// server.get('/home', function (request, response) {
-//     if (request.session.work !== undefined) {
-//         response.render('home'), {
-//            username: request.session.work.username,
-//            loginCount: request.session.work.logins,
-//            messages: messages,
-//        });
+server.post('/login', function(req, res) {
+  const user_login = req.body.username;
+  const user_password = req.body.password;
 
+  let signee = null;
 
+  for (let i = 0; i < users.length; i++) {
+    if (user_login === users[i].username &&
+      user_password === users[i].password) {
+      signee = users[i];
+    }
+  }
+  if (signee !== null) {
+    req.session.who = signee;
+    res.redirect('/home');
+  } else {
+    res.redirect('/');
+  }
+});
 
+server.get('/home', function(req, res) {
+  if (req.session.who !== undefined) {
+    res.render('home', {
+      username: req.session.who.username,
+    });
+  } else {
+    res.redirect('/');
+  }
+});
 
-server.listen(3003, function () {
-  console.log('Learn to code and be amazing and creative')
+server.get('/', function(req, res) {
+  const totalClicks = countClicks;
+
+  res.render('login',{
+    totalClicks: countClicks,
+  });
+});
+
+server.post('/log_Out', function(req, res) {
+  req.session.destroy(function(){
+    res.render('login', {
+      totalClicks: countClicks,
+    });
+  });
+});
+
+server.listen(3010, function(req, res){
+  console.log("GOING FOR A RUN");
 });
